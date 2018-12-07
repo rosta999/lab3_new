4fa0299a9cd3bde37a1eb13770c32caffe97678b
rosta999 <43803039+rosta999@users.noreply.github.com>
2018-12-07 09:03:08 -0800

    Update vm.c

--- vm.c
+++ vm.c
@@ -335,6 +335,22 @@ copyuvm(pde_t *pgdir, uint sz)
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
   }
+  
+  // copy stack
+	for(i = stack_top; i < USERTOP; i += PGSIZE){
+		if((pte = walkpgdir(pgdir, (void *) i, 1)) == 0)
+			panic("copyuvm: pte should exist");
+  		if(!(*pte & PTE_P))
+			panic("copyuvm: page not present");
+		pa = PTE_ADDR(*pte);
+		flags = PTE_FLAGS(*pte);
+		if((mem = kalloc()) == 0)
+			goto bad;
+		memmove(mem, (char*)P2V(pa), PGSIZE);
+		if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+			goto bad;
+ 	}
+
   return d;
 
 bad:
